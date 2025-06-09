fad295dfd46a0af490d8d8d035f88eb512581d62
MrRomero123 <luisromerop029@gmail.com>
2025-06-09 12:11:29 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -1,11 +1,9 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Cumbre Global sobre Cambio Climático: Nuevas Medidas Adoptadas",
+    "autor" => "Agencia Reuters",
+    "resumen" => "Líderes de más de 50 países han acordado implementar nuevas regulaciones 
+                  para reducir las emisiones de carbono en la próxima década. Expertos 
+                  consideran esta cumbre como un paso decisivo en la lucha contra el calentamiento global.",
 ];
 ?>
