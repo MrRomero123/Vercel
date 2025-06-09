336a0bea6adde42e832a056e45b4ba208f6bb798
MrRomero123 <luisromerop029@gmail.com>
2025-06-09 12:11:48 -0500

    Update nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -1,11 +1,10 @@
 <?php
-/*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
-*****/
-
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Nuevo Proyecto de Infraestructura Conectará Ciudades Clave",
+    "autor" => "Diario Nacional",
+    "resumen" => "El gobierno ha anunciado la construcción de una nueva autopista que 
+                  mejorará el transporte entre las principales ciudades del país. Se 
+                  espera que el proyecto genere miles de empleos y reduzca significativamente 
+                  los tiempos de viaje.",
 ];
 ?>
